ed61ba5810a86dce8b8965ea2b2234827a20ea4d
Elias J Manzueta <eliasmanz@gmail.com>
2019-03-26 23:56:25 -0400

    set up login route, appsettings, and userService

---
+++ src/app/user.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import * as AppSettings from '../appSettings.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+  
+}
