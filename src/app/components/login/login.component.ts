3336b82f3dbbdbd1a718e91450f8711e14b538df
Elias J Manzueta <eliasmanz@gmail.com>
2019-03-27 00:37:40 -0400

    login and redirect back to login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
   }
-
+  redirectToInstagram() {
+    this.userService.login();
+  }
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -8,4 +8,8 @@ export class UserService {
 
   constructor() { }
   
+  login() {
+    let redirectUrl = window.location.origin + '/login';
+    window.open(`https://api.instagram.com/oauth/authorize/?client_id=${AppSettings.API.clientId}&redirect_uri=${redirectUrl}&response_type=code`, '_self');
+  }
 }
