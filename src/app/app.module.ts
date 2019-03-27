8da3e8d7a72f0dda551333d26bb627cd41c53f59
Elias J Manzueta <eliasmanz@gmail.com>
2019-03-27 00:11:14 -0400

    restructure project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import * as AppSettings from '../appSettings.json';
+import * as AppSettings from '../../../appSettings.json';
 
 @Injectable({
   providedIn: 'root'
