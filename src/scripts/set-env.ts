7dee27025cb0ee24ffbb0d7adc6a66f7f0c996e4
Elias J Manzueta <eliasmanz@gmail.com>
2019-03-29 00:50:53 -0400

    force string env variable

--- src/scripts/set-env.ts
+++ src/scripts/set-env.ts
@@ -4,10 +4,10 @@ require('dotenv').config();
 const targetPath = `./src/appSettings.ts`;
 const envConfigFile = `
 export const AppSettings = {
-    clientId: ${process.env.CLIENT_ID},
-    clientSecret: ${process.env.CLIENT_SECRET},
-    url: ${process.env.API_URL},
-    authUrl: ${process.env.AUTH_URL}
+    clientId: '${process.env.CLIENT_ID}',
+    clientSecret: '${process.env.CLIENT_SECRET}',
+    url: '${process.env.API_URL}',
+    authUrl: '${process.env.AUTH_URL}'
 };
 `
 
