9cf397385b8b67275732ad55fe7486080b264635
Elias J Manzueta <eliasmanz@gmail.com>
2019-09-05 22:35:59 -0400

    Only display privacy policy link when not in url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { UserService } from './services/user/user.service';
+import { Router, RouterEvent, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,16 @@ import { UserService } from './services/user/user.service';
 })
 export class AppComponent {
   loading: boolean = false;
+  currentUrl: string = '';
   displayLoaderEvent: Subscription = new Subscription();
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private router: Router) {
     this.userService.displayLoaderEvent$.subscribe((displayLoader: boolean) => {
       this.loading = displayLoader;
+    });
+    router.events.subscribe((event: RouterEvent) => {
+      if (event instanceof NavigationEnd) {
+        this.currentUrl = event.url;
+      }
     });
   }
 }
